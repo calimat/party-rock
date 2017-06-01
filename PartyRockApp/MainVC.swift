a106f12b31ea15260b3bad4a0046ed5cb1e5e726
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-05-31 23:32:20 -0500

    added hard code data for testing UITableViewCell

--- PartyRockApp/MainVC.swift
+++ PartyRockApp/MainVC.swift
@@ -17,6 +17,15 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let imageUrlTest = "https://draw.acharts.net/artist/redfoo-54661f3e58d4e-l.png"
+        
+        let urlTest = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/dCFZ8XSpZSk\" frameborder=\"0\" allowfullscreen></iframe>"
+        
+        let p1 = PartyRock(imageURL: imageUrlTest, videoURL: urlTest, videoTitle: "RedFoo Sockin it to Ya 48 hour socks SALE")
+        
+        partyRocks.append(p1)
+        
         tableView.delegate = self
         tableView.dataSource = self
         
