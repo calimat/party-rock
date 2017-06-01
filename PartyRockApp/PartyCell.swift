70a549dd774c6c7e5d2d19943ab263f5a33c7f49
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-05-31 23:04:04 -0500

    custom cell (the right way to do it).

---
+++ PartyRockApp/PartyCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Ricardo Herrera Petit on 5/31/17.
+//  Copyright © 2017 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    @IBOutlet weak var videoTitle: UILabel!
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    override func awakeFromNib() {
+        
+    }
+
+    func updateUI() {
+        
+    }
+   
+
+}
